d928a14d996cd94bce66f88a729fae66ba56b9e1
home <sakpals@student.unimelb.edu.au>
2018-05-03 17:16:26 +1000

    Changed HTML for catsite

--- catsite/catrater/views.py
+++ catsite/catrater/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here. ("logic" of the application, passed to the template)
+def home_page(request):
+	return render(request, 'catrater/home_page.html', {})
+	
+
