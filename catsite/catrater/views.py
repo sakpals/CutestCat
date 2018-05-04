59491e1d1d02d3ca3f39626285bd4895302e95c4
home <sakpals@student.unimelb.edu.au>
2018-05-04 14:21:53 +1000

    templates display cats from database

--- catsite/catrater/views.py
+++ catsite/catrater/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Cat
+# Connects models and templates
 # Create your views here. ("logic" of the application, passed to the template)
 def home_page(request):
-	return render(request, 'catrater/home_page.html', {})
+	cats = Cat.objects.filter(upload_date__lte=timezone.now()).order_by('upload_date')
+	return render(request, 'catrater/home_page.html', {'cats': cats})
 	
 
